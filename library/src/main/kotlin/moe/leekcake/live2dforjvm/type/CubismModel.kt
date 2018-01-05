ed8d2770591d3ed2f493680f1cc81a714d2ec578
Blue Electric <fkdlxmslrx@gmail.com>
2018-01-05 21:35:54 +0900

    Make CubismModel to work

--- library/src/main/kotlin/moe/leekcake/live2dforjvm/type/CubismModel.kt
+++ library/src/main/kotlin/moe/leekcake/live2dforjvm/type/CubismModel.kt
@@ -1,13 +1,120 @@
 package moe.leekcake.live2dforjvm.type
 
+import moe.leekcake.live2dforjvm.Live2DCubismCoreJNI
 import moe.leekcake.live2dforjvm.Live2DCubismFrameworkJNI
+import moe.leekcake.live2dforjvm.MemoryAccessJNI
 
 /**
  * TODO: I don't sure about this class's doing in now :(
  *
  * Wrapping of csmModel(Core)
  */
-class CubismModel {
+class CubismModel(moc: CubismMoc) {
+    val pointer: Long
+    private fun finalize() {
+        MemoryAccessJNI.deAllocateAligned(pointer)
+    }
+
+    init {
+        val size = Live2DCubismCoreJNI.getSizeofModel(moc.pointer)
+        pointer = MemoryAccessJNI.allocateAligned(size.toInt(), Alignment.model)
+        Live2DCubismCoreJNI.initializeModelInPlace(moc.pointer, pointer, size)
+    }
+
+    fun update() = Live2DCubismCoreJNI.updateModel(pointer)
+
+    data class CanvasInfo(val sizeInPixels: Vector2, val originInPixels: Vector2, val pixelsPerUnit: Float)
+    fun readCanvasInfo(): CanvasInfo {
+        val sizeInPixels = Vector2()
+        val originInPixels = Vector2()
+
+        val pixelsPerUnit = Live2DCubismCoreJNI.readCanvasInfo(pointer, sizeInPixels.pointer, originInPixels.pointer)
+        return CanvasInfo(sizeInPixels, originInPixels, pixelsPerUnit)
+    }
+
+    inner class Parameter(val index: Int) {
+        val id: String = Live2DCubismCoreJNI.getParameterId(pointer, index)
+        val minimumValue: Float
+            get() = Live2DCubismCoreJNI.getParameterMinimumValue(pointer, index)
+        val maximumValue: Float
+            get() = Live2DCubismCoreJNI.getParameterMaximumValue(pointer, index)
+        val defaultValue: Float
+            get() = Live2DCubismCoreJNI.getParameterDefaultValue(pointer, index)
+        val value: Float
+            get() = Live2DCubismCoreJNI.getParameterValue(pointer, index)
+    }
+    inner class ParameterProvider {
+        operator fun get(index: Int) = Parameter(index)
+
+        val size: Int = Live2DCubismCoreJNI.getParameterCount(pointer)
+    }
+    val parameters = ParameterProvider()
+
+    inner class Part(val index: Int) {
+        val id: String = Live2DCubismCoreJNI.getParameterId(pointer, index)
+        val opacity: Float
+            get() = Live2DCubismCoreJNI.getPartOpacity(pointer, index)
+    }
+    inner class PartProvider {
+        operator fun get(index: Int) = Part(index)
+
+        val size: Int = Live2DCubismCoreJNI.getPartCount(pointer)
+    }
+    val parts = PartProvider()
+
+    inner class Drawable(val index: Int) {
+        val id: String = Live2DCubismCoreJNI.getDrawableId(pointer, index)
+        val constantFlag: Int
+            get() = Live2DCubismCoreJNI.getDrawableConstantFlag(pointer, index)
+        val dynamicFlag: Int
+            get() = Live2DCubismCoreJNI.getDrawableDynamicFlag(pointer, index)
+        val textureIndex: Int
+            get() = Live2DCubismCoreJNI.getDrawableTextureIndex(pointer, index)
+        val drawOrder: Int
+            get() = Live2DCubismCoreJNI.getDrawableDrawOrder(pointer, index)
+        val renderOrder: Int
+            get() = Live2DCubismCoreJNI.getDrawableRenderOrder(pointer, index)
+        val opacity: Float
+            get() = Live2DCubismCoreJNI.getDrawableOpacity(pointer, index)
+
+        inner class MaskProvider {
+            operator fun get(index: Int) = Live2DCubismCoreJNI.getDrawableMask(pointer, this@Drawable.index, index)
+
+            val size: Int = Live2DCubismCoreJNI.getDrawableMaskCount(pointer, index)
+        }
+        val masks = MaskProvider()
+
+        inner class Vertex(val index: Int) {
+            val position = Vector2( Live2DCubismCoreJNI.getDrawableVertexPosition(pointer, this@Drawable.index, index) )
+            val uv = Vector2( Live2DCubismCoreJNI.getDrawableVertexUv(pointer, this@Drawable.index, index) )
+        }
+
+        inner class VertexProvider {
+            operator fun get(index: Int) = Vertex(index)
+
+            val size: Int = Live2DCubismCoreJNI.getDrawableVertexCount(pointer, index)
+        }
+        val vertices = VertexProvider()
+
+        inner class IndexProvider {
+            operator fun get(index: Int) = Live2DCubismCoreJNI.getDrawableIndex(pointer, this@Drawable.index, index)
+
+            val size: Int = Live2DCubismCoreJNI.getDrawableIndexCount(pointer, this@Drawable.index)
+        }
+        val indices = IndexProvider()
+    }
+
+    inner class DrawableProvider {
+        operator fun get(index: Int) = Drawable(index)
+
+        val size: Int = Live2DCubismCoreJNI.getDrawableCount(pointer)
+    }
+    val drawables = DrawableProvider()
+
+    fun resetDrawableDynamicFlags() {
+        Live2DCubismCoreJNI.resetDrawableDynamicFlags(pointer)
+    }
+
     object AnimationCurveType {
         val opacityAnimationCurve = 0
         val eyeBlinkAnimationCurve = 1
