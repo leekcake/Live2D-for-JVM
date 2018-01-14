6f169ee66774e6c9bab0726987a49a4dd8ed6a01
Blue Electric <fkdlxmslrx@gmail.com>
2018-01-14 20:15:47 +0900

    Add android sample

---
+++ sample-android/src/main/java/moe/leekcake/hellocubism/MainActivity.kt
@@ -0,0 +1,146 @@
+package moe.leekcake.hellocubism
+
+import android.app.Activity
+import android.opengl.GLES20
+import android.opengl.GLES20.GL_COLOR_BUFFER_BIT
+import android.opengl.GLES20.GL_DEPTH_BUFFER_BIT
+import android.os.Bundle
+import android.opengl.GLSurfaceView
+import moe.leekcake.live2dforjvm.Live2DCubismFrameworkJNI
+import moe.leekcake.live2dforjvm.Live2DCubismGLRenderingJNI
+import moe.leekcake.live2dforjvm.type.*
+import java.io.File
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+
+class MainActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        glView = GLSurfaceView(this)
+        glView.setEGLContextClientVersion(2)
+        glView.setPreserveEGLContextOnPause(true)
+        glView.setRenderer(Renderer())
+
+        setContentView(glView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        glView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glView.onResume()
+    }
+
+    private fun generateViewProjection(safeFrame: Float): FloatArray {
+        var aspect: Float
+        val xScale: Float
+        val yScale: Float
+
+        val windowWidth = width
+        val windowHeight = height
+        aspect = windowWidth / windowHeight
+
+        if (aspect > 1.0f) {
+            yScale = 1.0f / safeFrame * 2.0f
+            xScale = yScale * (1.0f / aspect)
+        } else {
+            aspect = windowHeight / windowWidth
+
+            xScale = 1.0f / safeFrame * 2.0f
+            yScale = xScale * (1.0f / aspect)
+        }
+
+        val vp = FloatArray(16)
+        // Set matrix.
+        vp[0] = xScale; vp[1] = 0.0f; vp[2] = 0.0f; vp[3] = 0.0f
+        vp[4] = 0.0f; vp[5] = yScale; vp[6] = 0.0f; vp[7] = 0.0f
+        vp[8] = 0.0f; vp[9] = 0.0f; vp[10] = 1.0f; vp[11] = 0.0f
+        vp[12] = 0.0f; vp[13] = 0.0f; vp[14] = 0.0f; vp[15] = 1.0f
+
+        return vp
+    }
+
+    private lateinit var glView: GLSurfaceView
+
+    private lateinit var moc: CubismMoc
+    private lateinit var model: CubismModel
+    private lateinit var table: CubismModelHashTable
+
+    private lateinit var animation: CubismAnimation
+    private lateinit var animationState: CubismAnimationState
+
+    private lateinit var renderer: CubismGLRenderer
+
+    //private lateinit var texture: Texture
+    private var texture: Int = 0
+
+    private lateinit var vp: FloatArray
+
+    private var lastTime: Long = 0
+
+    private var width = 720f
+    private var height = 1280f
+
+    private inner class Renderer: GLSurfaceView.Renderer {
+        override fun onDrawFrame(p0: GL10?) {
+            GLES20.glEnable(GLES20.GL_BLEND)
+            GLES20.glDisable(GLES20.GL_DEPTH_TEST)
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1f); // Black Background
+            GLES20.glClearDepthf(1.0f); // Depth Buffer Setup
+            GLES20.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA)
+
+            val delta = (System.currentTimeMillis() - lastTime).toFloat() / 1000f
+            lastTime = System.currentTimeMillis();
+
+            animationState.update(delta)
+            animation.evaluate(animationState, Live2DCubismFrameworkJNI.getOverrideFloatBlendFunction(),
+                    1f, model, table, CubismModel.AnimationCurveType.OpacityAnimationCurve, 0)
+
+            model.update()
+            renderer.update()
+
+            model.resetDrawableDynamicFlags()
+
+            renderer.draw(vp, texture.toLong())
+        }
+
+        override fun onSurfaceChanged(p0: GL10?, p1: Int, p2: Int) {
+
+        }
+
+        override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
+            width = glView.measuredWidth.toFloat()
+            height = glView.measuredHeight.toFloat()
+
+            Live2DCubismGLRenderingJNI.ensureGLAD()
+
+            val stream = applicationContext.assets.open("Koharu.moc3")
+            moc = CubismMoc(stream, stream.available())
+            stream.close()
+
+            model = CubismModel(moc)
+            table = CubismModelHashTable(model)
+
+            val jsonStream = application.assets.open("Koharu.motion3.json")
+            val motionJson = String( jsonStream.readBytes(jsonStream.available()) )
+            jsonStream.close()
+
+            animation = CubismAnimation( motionJson )
+            animationState = CubismAnimationState()
+
+            renderer = CubismGLRenderer(model)
+
+            val textureStream = application.assets.open("Koharu.png")
+            texture = Utils.loadTexture( textureStream )
+            textureStream.close()
+
+            vp = generateViewProjection(2f)
+            GLES20.glViewport(0, 0, width.toInt(), height.toInt())
+            lastTime = System.currentTimeMillis()
+        }
+    }
+}
---
+++ sample-android/src/main/java/moe/leekcake/hellocubism/Utils.kt
@@ -0,0 +1,75 @@
+package moe.leekcake.hellocubism
+
+import android.content.Context
+import android.opengl.GLUtils
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.opengl.GLES20
+import android.opengl.GLES30
+import android.opengl.GLES32
+import java.io.InputStream
+import javax.microedition.khronos.opengles.GL
+import java.nio.ByteOrder.BIG_ENDIAN
+import android.R.attr.order
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import android.R.attr.bitmap
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.util.Log
+
+
+object Utils {
+
+
+    //From http://gogorchg.tistory.com/entry/Android-Opengl-es-20-LoadTexture
+    //Tweaked
+    fun loadTexture(stream: InputStream): Int {
+        val textureId = IntArray(1)
+        val bitmap: Bitmap
+        bitmap = BitmapFactory.decodeStream(stream)
+
+        val byteBuffer = ByteBuffer.allocateDirect(bitmap.width * bitmap.height * 4)
+        byteBuffer.order(ByteOrder.BIG_ENDIAN)
+        val ib = byteBuffer.asIntBuffer()
+
+        val pixels = IntArray(bitmap.width * bitmap.height)
+        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        /*
+        for (i in pixels.indices) {
+            ib.put(pixels[i] shl 8 or pixels[i].ushr(24))
+        }
+        */
+        //Flip Y for OpenGL using
+        val forHeight = bitmap.height - 1
+        for(y in 0 until bitmap.height) {
+            for(x in 0 until bitmap.width) {
+                val value = pixels[ x + (y * bitmap.width) ]
+                try {
+                    ib.put(x + ((forHeight - y) * bitmap.height), value.shl(8) or value.ushr(24))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    Log.e("xy", "$x . $y")
+                    throw e
+                }
+            }
+        }
+
+        bitmap.recycle()
+
+        byteBuffer.position(0)
+        GLES20.glGenTextures(1, textureId, 0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId[0])
+
+        GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, bitmap.width, bitmap.height, 0,
+                GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, byteBuffer)
+
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+
+        return textureId[0]
+    }
+}
