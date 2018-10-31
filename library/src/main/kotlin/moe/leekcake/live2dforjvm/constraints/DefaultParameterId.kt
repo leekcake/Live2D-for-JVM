e4e1cf0e4539b439ab923f62ed597730fe45fceb
Blue Electric <fkdlxmslrx@gmail.com>
2018-10-31 22:10:36 +0900

    Import DefaultParameterId

---
+++ library/src/main/kotlin/moe/leekcake/live2dforjvm/constraints/DefaultParameterId.kt
@@ -0,0 +1,56 @@
+package moe.leekcake.live2dforjvm.constraints
+
+//CubismDefaultParameterId
+object DefaultParameterId {
+    // パーツID
+    val HitAreaPrefix = "HitArea";
+    val HitAreaHead = "Head";
+    val HitAreaBody = "Body";
+    val PartsIdCore = "Parts01Core";
+    val PartsArmPrefix = "Parts01Arm_";
+    val PartsArmLPrefix = "Parts01ArmL_";
+    val PartsArmRPrefix = "Parts01ArmR_";
+
+    // パラメータID
+    val ParamAngleX = "ParamAngleX";
+    val ParamAngleY = "ParamAngleY";
+    val ParamAngleZ = "ParamAngleZ";
+    val ParamEyeLOpen = "ParamEyeLOpen";
+    val ParamEyeLSmile = "ParamEyeLSmile";
+    val ParamEyeROpen = "ParamEyeROpen";
+    val ParamEyeRSmile = "ParamEyeRSmile";
+    val ParamEyeBallX = "ParamEyeBallX";
+    val ParamEyeBallY = "ParamEyeBallY";
+    val ParamEyeBallForm = "ParamEyeBallForm";
+    val ParamBrowLY = "ParamBrowLY";
+    val ParamBrowRY = "ParamBrowRY";
+    val ParamBrowLX = "ParamBrowLX";
+    val ParamBrowRX = "ParamBrowRX";
+    val ParamBrowLAngle = "ParamBrowLAngle";
+    val ParamBrowRAngle = "ParamBrowRAngle";
+    val ParamBrowLForm = "ParamBrowLForm";
+    val ParamBrowRForm = "ParamBrowRForm";
+    val ParamMouthForm = "ParamMouthForm";
+    val ParamMouthOpenY = "ParamMouthOpenY";
+    val ParamCheek = "ParamCheek";
+    val ParamBodyAngleX = "ParamBodyAngleX";
+    val ParamBodyAngleY = "ParamBodyAngleY";
+    val ParamBodyAngleZ = "ParamBodyAngleZ";
+    val ParamBreath = "ParamBreath";
+    val ParamArmLA = "ParamArmLA";
+    val ParamArmRA = "ParamArmRA";
+    val ParamArmLB = "ParamArmLB";
+    val ParamArmRB = "ParamArmRB";
+    val ParamHandL = "ParamHandL";
+    val ParamHandR = "ParamHandR";
+    val ParamHairFront = "ParamHairFront";
+    val ParamHairSide = "ParamHairSide";
+    val ParamHairBack = "ParamHairBack";
+    val ParamHairFluffy = "ParamHairFluffy";
+    val ParamShoulderY = "ParamShoulderY";
+    val ParamBustX = "ParamBustX";
+    val ParamBustY = "ParamBustY";
+    val ParamBaseX = "ParamBaseX";
+    val ParamBaseY = "ParamBaseY";
+    val ParamNONE = "NONE:";
+}
